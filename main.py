0fa9068f841699b4c4bde97316f9b0aa8e8c9a48
Carlos Dávila <dev.carlos@hotmail.com>
2025-05-11 12:35:50 -0500

    change model

--- main.py
+++ main.py
@@ -19,9 +19,11 @@ if not firebase_admin._apps:
 db = firestore.client()
 
 # Cargar modelo de Hugging Face
-model_name = "csdavila/Llama-3.2.C"
+model_name = "openlm-research/open_llama_3b"
+# model_name = "csdavila/Llama-3.2.C"
 # model_name = "meta-llama/Llama-3.2-1B"
-tokenizer  = AutoTokenizer.from_pretrained(model_name, device_map="auto", use_safetensors=True)
+# tokenizer  = AutoTokenizer.from_pretrained(model_name, device_map="auto", use_safetensors=True)
+tokenizer  = AutoTokenizer.from_pretrained(model_name)
 device     = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model      = AutoModelForCausalLM.from_pretrained(
     model_name,
